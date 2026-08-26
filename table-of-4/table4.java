cf1616537f17e2fbad83ad262ff8f378ddec1f28
anandsinghrocker234-debug <anandsinghrocker234@gmail.com>
2026-08-26 21:28:32 +0530

    added table of 4

---
+++ table-of-4/table4.java
@@ -0,0 +1,7 @@
+class Table {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("4 x " + i + " = " + (4 * i));
+        }
+    }
+}
